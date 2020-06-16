6e81bda096c6fb9f81fa5554a775f11871cc39b3
Linus Alpsten <linus.alpsten@telia.com>
2020-06-16 16:50:05 +0200

    nums

--- Random/Program.cs
+++ Random/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
 
 namespace Random
 {
@@ -7,8 +8,28 @@ namespace Random
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(DateTime.Now.Ticks);
-            Console.WriteLine(Process.GetCurrentProcess().PrivateMemorySize64);
+            Console.WriteLine(ThreeLastDigits(DateTime.Now.Ticks));
+            string numsX = "";
+            string numsY = "";
+            string numsZ = "";
+            for (int i = 0; i < 10000; i++)
+            {
+                numsX += ThreeLastDigits(DateTime.Now.Ticks) + " ";
+                numsY += ThreeLastDigits(DateTime.Now.Ticks) + " ";
+                numsZ += ThreeLastDigits(DateTime.Now.Ticks) + " ";
+            }
+            string path = @"C:\Users\jagme\source\repos\Random\Random\nums.txt";
+            using (StreamWriter sw = File.CreateText(path))
+            {
+                sw.WriteLine(numsX);
+                sw.WriteLine(numsY);
+                sw.WriteLine(numsZ);
+            }
+        }
+
+        static int ThreeLastDigits(long num)
+        {
+           return Convert.ToInt32(num % 1000);
         }
     }
 }
